3430c0368a73e2b1b7ecf399a031e70291237054
mishleen <michealmishleen@gmail.com>
2024-09-26 15:36:58 +0300

    Create Custom Note Item

--- lib/weidget/notes_view_body.dart
+++ lib/weidget/notes_view_body.dart
@@ -1,19 +1,23 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import 'custom_appbar.dart';
+import 'custom_notes_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
           SizedBox(height: 50,),
           CustomAppBar(),
+          NotesItem(),
+          
         ],
       ),
     );
