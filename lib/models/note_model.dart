c84cf74837816712537e3744f49ac5f972ba2823
mishleen <michealmishleen@gmail.com>
2024-09-26 20:12:22 +0300

    create Note Model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,7 @@
+class NoteModel{
+ final String title;
+ final String subTitle;
+ final String date;
+ final int color;
+  NoteModel({required this.subTitle, required this.date, required this.color, required this.title, } );
+}
