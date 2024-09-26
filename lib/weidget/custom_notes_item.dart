0820356607a8f449d784f57a5139a4c801af30c0
mishleen <michealmishleen@gmail.com>
2024-09-26 18:44:38 +0300

    Create Edit Note View

--- lib/weidget/custom_notes_item.dart
+++ lib/weidget/custom_notes_item.dart
@@ -1,47 +1,61 @@
-
 import 'package:flutter/material.dart';
+import 'package:notes_app/view/edit_note_view.dart';
+
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 24,bottom: 24, left: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text("Flutter tips"
-                , style: TextStyle(
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Text(
+                  "Flutter tips",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 16),
+                  child: Text(
+                    "Build your Career with Mishleen Micheal",
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(.4), fontSize: 20),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete),
                   color: Colors.black,
-                  fontSize: 28
-                  ,
-                ),),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16 , bottom: 16),
-                child: Text("Build your Career with Mishleen Micheal"
-                  , style: TextStyle(
-                      color: Colors.black.withOpacity(.4),
-                      fontSize: 20
-                  ),),
+                  iconSize: 25,
+                ),
               ),
-              trailing: IconButton( onPressed: (){},icon: const Icon (Icons.delete) , color: Colors.black,iconSize: 25,),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right:  24),
-              child: Text("May 21.2022"
-                , style: TextStyle(
-                    color: Colors.black.withOpacity(.4),
-                    fontSize: 18
-                ),),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
+                child: Text(
+                  "May 21.2022",
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.4), fontSize: 18),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
